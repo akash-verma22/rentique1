eb1228e227defaea4fc0ef8772b5a4abde538721
akash-verma22 <akashvverma01@gmail.com>
2025-07-21 20:34:07 +0530

    Add final configurations

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.export = {
+    apps:[
+        {
+            name: "project-management",
+            scripts: "npm",
+            args: "run dev",
+            env: {
+                NODE_ENV: "development",
+            }
+        }
+    ]    
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use("/leases",leaseRoutes);
 app.use("/applications",applicationRoutes);
 
 /* SERVER */
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3002;
+app.listen(port,"0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
