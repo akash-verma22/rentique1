6f2f768ef6e4ba395200ebd9c3bdd99283f0a488
Akash Verma <akashvverma01@gmail.com>
2025-07-24 09:41:55 +0530

    Update propertyRoutes.ts

--- server/src/routes/propertyRoutes.ts
+++ server/src/routes/propertyRoutes.ts
@@ -8,12 +8,13 @@ import multer from "multer";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 const storage = multer.memoryStorage();
-const upload = multer({storage:storage});
+const upload = multer({storage:storage,
+                      limits:{fileSize:10*1024*1024});
 
 const router = express.Router();
 
 router.get("/", getProperties);
 router.get("/:id", getProperty);
-router.post("/", authMiddleware(["manager"]),upload.array("photos"), createProperty);
+router.post("/", authMiddleware(["manager"]),upload.array("photos",10), createProperty);
 
 export default router;
